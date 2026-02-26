1e0384c2a1208dd4650f35de644800c30855a640
Gabriele G Vieira <gabrielevieira011@gmail.com>
2026-02-25 21:31:39 -0300

    feat(home): created a initial page and a home after login

---
+++ app/home/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { Button, Space } from "antd";
+import { useRouter } from "next/navigation";
+
+export default function HomePage() {
+  const router = useRouter()
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "90vh",
+      }}
+    >
+      <Space orientation="vertical" size="large" >
+        <Button size="large" style={{ width: 220 }} onClick={() => router.push("/album")}>Album</Button>
+        <Button size="large" style={{ width: 220 }} onClick={() => router.push("/stickers")}>Figurinhas</Button>
+        <Button size="large" style={{ width: 220 }}>Ranking</Button>
+      </Space>
+    </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,37 @@
-"use client"
+"use client";
 
-import { useRouter } from "next/navigation"
-import { useEffect } from "react"
-import { useAuthInit } from "@/hooks/useAuthInit"
-import { useUserProfile } from "@/hooks/useUserProfile"
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { useAuthInit } from "@/hooks/useAuthInit";
+import { useUserProfile } from "@/hooks/useUserProfile";
+import { LoadingOutlined } from "@ant-design/icons";
+import { Spin } from "antd";
 
-export default function Home() {
-  const router = useRouter()
-  const { loading: authLoading } = useAuthInit()
-  const { profile, loading: profileLoading } = useUserProfile()
+export default function Login() {
+  const router = useRouter();
+  const { loading: authLoading } = useAuthInit();
+  const { profile, loading: profileLoading } = useUserProfile();
 
   useEffect(() => {
-    if (authLoading || profileLoading) return
+    if (authLoading || profileLoading) return;
 
     if (!profile?.parish_id) {
-      router.replace("/onboarding")
+      router.replace("/onboarding");
     } else {
-      router.replace("/album")
+      router.replace("/home");
     }
-  }, [authLoading, profileLoading, profile, router])
+  }, [authLoading, profileLoading, profile, router]);
 
-  return <div>Carregando...</div>
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "90vh",
+      }}
+    >
+          <Spin indicator={<LoadingOutlined style={{ fontSize: 48 }} spin />} />
+    </div>
+  );
 }
